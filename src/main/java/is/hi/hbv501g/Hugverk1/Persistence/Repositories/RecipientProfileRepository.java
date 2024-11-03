6ca00c15011cc34b4ad5bdc508ff0eb1de506b51
Guðrún Ísabella Kjartansdóttir <gik10@hi.is>
2024-11-03 23:31:13 +0000

    match service for recipient and donor(start)

--- src/main/java/is/hi/hbv501g/Hugverk1/Persistence/Repositories/RecipientProfileRepository.java
+++ src/main/java/is/hi/hbv501g/Hugverk1/Persistence/Repositories/RecipientProfileRepository.java
@@ -1,7 +1,8 @@
 package is.hi.hbv501g.Hugverk1.Persistence.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipientProfileRepository {
+public interface RecipientProfileRepository extends JpaRepository<RecipientProfile, Long> {
 }
