b0de6df93f3af9b1aee4cdf3cce71076bbe8a2a9
alexandrabjorkm <abm18@hi.is>
2024-11-16 16:08:29 +0000

    update donor limit endpont

--- src/main/java/is/hi/hbv501g/Hugverk1/controller/AdminHomeController.java
+++ src/main/java/is/hi/hbv501g/Hugverk1/controller/AdminHomeController.java
@@ -1,6 +1,8 @@
 package is.hi.hbv501g.Hugverk1.controller;
 
 import is.hi.hbv501g.Hugverk1.Persistence.Entities.MyAppUsers;
+import is.hi.hbv501g.Hugverk1.Persistence.Entities.DonorProfile;
+import is.hi.hbv501g.Hugverk1.Services.DonorProfileService;
 import is.hi.hbv501g.Hugverk1.Services.MyAppUserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,9 @@ public class AdminHomeController {
     @Autowired
     private MyAppUserService myAppUserService;
 
+    @Autowired
+    private DonorProfileService donorProfileService;
+
     @GetMapping("/home/admin")
     public String adminHome(Model model, HttpSession session) {
         MyAppUsers loggedInUser = (MyAppUsers) session.getAttribute("LoggedInUser");
@@ -27,5 +32,17 @@ public class AdminHomeController {
         model.addAttribute("username", loggedInUser.getUsername());
         model.addAttribute("recipientsWhoFavorited", recipientsWhoFavorited);
         return "adminHome";
+    }
+    @GetMapping("/donorlimits")
+    public String viewDonorLimits(Model model, HttpSession session) {
+        MyAppUsers loggedInUser = (MyAppUsers) session.getAttribute("LoggedInUser");
+        if (loggedInUser == null || !"admin".equalsIgnoreCase(loggedInUser.getUserType())) {
+            return "redirect:/users/login";
+        }
+
+        List<DonorProfile> allDonors = donorProfileService.getAllDonors();
+        model.addAttribute("allDonors", allDonors);
+
+        return "donorLimits";
     }
 }
